e3d76cbc48816eaee25cb5124a7f772bc3210e45
jamiroph <james@jamiroph.co.uk>
2023-01-12 20:48:09 +0000

    Implement tailwind css

--- assets/js/components/Layout.vue
+++ assets/js/components/Layout.vue
@@ -3,10 +3,10 @@ import { Link } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
-    <nav>
+    <nav class="flex text-teal-500 gap-4 justify-items-end">
         <ul>
-            <li><Link :href="$route('home')">Home</Link></li>
-            <li><Link :href="$route('contact')">Contact</Link></li>
+            <li class="text-3xl font-bold underline"><Link :href="$route('home')">Home</Link></li>
+            <li class="text-3xl font-bold underline"><Link :href="$route('contact')">Contact</Link></li>
         </ul>
     </nav>
     <main>
