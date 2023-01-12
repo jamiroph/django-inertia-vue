e1712e88068391a6444e19b2c9f0043ffef3facc
jamiroph <james@jamiroph.co.uk>
2023-01-12 21:53:55 +0000

    Fix link issues, start formating additional elements

--- assets/js/components/MainNav.vue
+++ assets/js/components/MainNav.vue
@@ -9,7 +9,7 @@ import { Link } from "@inertiajs/inertia-vue3";
 		<div
 			class="container flex flex-wrap items-center justify-between mx-auto"
 		>
-			<a href="https://flowbite.com/" class="flex items-center">
+			<Link :href="$route('home')" class="flex items-center">
 				<img
 					src="https://flowbite.com/docs/images/logo.svg"
 					class="h-6 mr-3 sm:h-9"
@@ -19,7 +19,7 @@ import { Link } from "@inertiajs/inertia-vue3";
 					class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
 					>Flowbite</span
 				>
-			</a>
+            </Link>
 			<div class="flex md:order-2">
 				<button
 					type="button"
@@ -58,32 +58,32 @@ import { Link } from "@inertiajs/inertia-vue3";
 					class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
 				>
 					<li>
-						<a
-							href="#"
+						<Link
+							:href="$route('home')"
 							class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
 							aria-current="page"
-							>Home</a
+							>Home</Link
 						>
 					</li>
 					<li>
-						<a
+						<Link
 							href="#"
 							class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>About</a
+							>About</Link
 						>
 					</li>
 					<li>
-						<a
+						<Link
 							href="#"
 							class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>Services</a
+							>Services</Link
 						>
 					</li>
 					<li>
-						<a
-							href="#"
+						<Link
+							:href="$route('contact')"
 							class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>Contact</a
+							>Contact</Link
 						>
 					</li>
 				</ul>
--- assets/js/pages/Polls/Index.vue
+++ assets/js/pages/Polls/Index.vue
@@ -1,24 +1,26 @@
 <script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
 const props = defineProps({
     polls: Object
 });
 </script>
 
 
-<template>
-    <h2 class="m-auto">This is the polls page</h2>
-    <div class="container lg-8 m-auto">
-        <a v-for="(poll, index) in polls" :key="index"
-			href="#"
-			class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-		>
-			<h5 v-if="poll.title"
-				class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-			>{{ poll.title }}
-			</h5>
-			<p class="font-normal text-gray-700 dark:text-gray-400">
-				{{ poll.question_text }}
-			</p>
-		</a>
+<template> 
+    <div class="container mx-auto">
+        <h2 class="text-3xl capitalize text-teal-600">Polls</h2>
+        <div v-for="(poll, index) in polls" :key="index" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <Link :href="$route('home')">
+                <h5 v-if="poll.title"
+                    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                >{{ poll.title }}
+                </h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400">
+                    {{ poll.question_text }}
+                </p>
+		    </Link>
+        </div>
+        
     </div>
 </template>
